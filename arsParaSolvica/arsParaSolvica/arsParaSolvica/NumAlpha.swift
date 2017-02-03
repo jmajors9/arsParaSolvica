f6b47934d41bf8aac3898a9d8a2ada5186cb9ffa
Joel Majors <jmajors9@users.noreply.github.com>
2017-02-03 14:47:21 -0500

    Added files via upload
    Added initial files.
    None of the javascript is my own. Pulled from various sites.

---
+++ arsParaSolvica/arsParaSolvica/arsParaSolvica/NumAlpha.swift
@@ -0,0 +1,40 @@
+//
+//  NumAlpha.swift
+//  arsParaSolvica
+//
+//  Created by Joel Majors on 2/2/17.
+//  Copyright © 2017 GloryKiddTech. All rights reserved.
+//
+
+import Foundation
+
+// need to intake numbers and change to letters
+    // need to change
+    // need to change letters based on A=1 
+    // need to change letters based on A=26
+
+// create an array for a-z, let a=0, then add 1. Or sub 1 from numnbers provided?
+
+protocol Letters {
+
+}
+
+
+struct NumAlpha {
+
+    let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
+    
+    
+    func numAlpha(type: Int) -> String {
+        var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        var numbers = "0123456789"
+        var out: String
+        var input = 0
+        
+        return
+        
+    }
+
+    
+}
---
+++ arsParaSolvica/arsParaSolvica/arsParaSolvica/Vigenere.swift
@@ -0,0 +1,38 @@
+//
+//  Vigenere.swift
+//  arsParaSolvica
+//
+//  Created by Joel Majors on 2/2/17.
+//  Copyright © 2017 GloryKiddTech. All rights reserved.
+//
+
+import Foundation
+/*--------------------------------------------------
+write two functions:
+
+1. GetKeys
+    new array
+    get input
+        handle error of nil input
+   
+    a = convert to uppercase
+    for (var i=0; i< a.length; i++) {
+        m[i] = a.charCodeAt(i) - 65;
+    }  why minus 65?
+
+
+2. decode
+    retrieve from GetKeys
+    
+    var to create new array of the input?
+    convert array to uppercase
+    trim to remove whitespace?
+ 
+ 
+     for (i=0; i < k1.length;  i++) {
+         k[i] = (26-k1[i]); 
+     }
+ 
+ 
+ 
+ --------------------------------------------------*/
